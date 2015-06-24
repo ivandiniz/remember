a149dad307e10df492de03cbe25d03f8ec6d3492
Ivan Diniz <ivandiniz@gmail.com>
2015-06-23 23:10:46 -0300

    commit apos a criação do welcome_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :friends
   resources :annotations
   # The priority is based upon order of creation: first created -> highest priority.
